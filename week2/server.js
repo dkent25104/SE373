a7eba289f04e91ba7c47d63997144063e8617ece
Kent <dkent@labs.neit.edu>
2020-01-13 21:02:42 -0500

    week 2

---
+++ week2/server.js
@@ -0,0 +1,22 @@
+var express = require("express");
+//var path = require("path");
+var hbs = require("hbs");
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+
+app.use(express.urlencoded());
+app.use(express.static(__dirname + '/public'));
+app.set('view engine', 'hbs');
+
+app.get("/index", (req,res)=>{   //or function(){} instead of ()=>{}
+    //res.sendFile(path.join(__dirname, "/public/index.html"));
+    res.render("index.hbs", {junk:"My name is Bobert."});
+});
+
+app.get("/results", (req,res)=>{   //or function(){} instead of ()=>{}
+    //res.sendFile(path.join(__dirname, "/public/index.html"));
+    res.render("results.hbs", {first:req.body.first, last:req.body.last});
+});
+
+app.listen(3000, ()=>{console.log("server running on port 3000")});
